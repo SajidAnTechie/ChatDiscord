3ccb3c3810630bdc2f9daa4f13e2bc883cd10f92
Sajid Ansari <sajidansari33272@gmail.com>
2020-07-22 15:59:42 +0545

    emoji-mart added

--- client/src/components/input/Input.js
+++ client/src/components/input/Input.js
@@ -1,18 +1,29 @@
 import React, { useState } from "react";
+import "emoji-mart/css/emoji-mart.css";
+import { Picker } from "emoji-mart";
 
 const InputContainer = ({ sendMessage }) => {
   const [text, settext] = useState("");
+  const [showEmoji, setEmoji] = useState(false);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    const message = text.trim().length;
 
-    if (message === 0) {
+    if (text.trim().length === 0) {
       return;
     }
 
-    sendMessage(message);
+    sendMessage(text.trim());
     settext("");
+  };
+
+  const addEmoji = (e) => {
+    // console.log(e.native);
+    let emoji = e.native;
+    settext(text + emoji);
+  };
+  const toogleEmoji = () => {
+    setEmoji(!showEmoji);
   };
 
   return (
@@ -31,6 +42,30 @@ const InputContainer = ({ sendMessage }) => {
           <i className="fas fa-paper-plane"></i> Send
         </button>
       </form>
+
+      {showEmoji && (
+        <span>
+          <div style={{ margin: "10px" }}>
+            <button
+              style={{
+                width: "50px",
+                height: "30px",
+                border: "none",
+                borderRadius: "3px",
+              }}
+              onClick={toogleEmoji}
+            >
+              Close
+            </button>
+          </div>
+          <Picker onSelect={addEmoji} emojiTooltip={true} title="CharDiscord" />
+        </span>
+      )}
+      {!showEmoji && (
+        <button style={{ margin: "10px" }} onClick={toogleEmoji}>
+          {String.fromCodePoint(0x1f60a)}
+        </button>
+      )}
     </div>
   );
 };
